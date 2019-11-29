59691612515484bbbdda6b9ff3bb3641c01b5f58
Dino Wang <dino.cool@gmail.com>
2019-11-29 14:19:14 +0800

    initial

---
+++ Microsoft docuemnts language (zh-tw-en-us) switcher.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Microsoft docuemnts language (zh-tw/en-us) switcher
+// @namespace    http://dinowang.net/
+// @version      0.1
+// @description
+// @author       Dino Wang
+// @match        https://*.microsoft.com/*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    (function () {
+        var langs = [ "zh-tw", "zh-cn", "en-us" ],
+            pathParts = location.pathname.split(/\//g),
+            currentLang = pathParts[1];
+
+        var style = document.createElement('style');
+        style.type = "text/css";
+        style.innerHTML = ".msex-container { position: fixed; bottom: 0px; right: 0px; background-color: #333; text-align: center; padding: 0 8px; }" +
+                          ".msex-container a { font-size: 9pt; }" +
+                          ".msex-container a:not(:last-child):after { content: \"|\"; margin: 0 4px; color: #777; }";
+        document.getElementsByTagName('head')[0].appendChild(style);
+
+        if (currentLang && currentLang.match(/^(zh-tw|zh-cn|en-us)/)) {
+            var container = document.createElement("div");
+            container.className = "msex-container";
+            document.body.insertBefore(container, document.body.children[0]);
+
+            var toogleButton = document.querySelector("#language-toggle");
+            if (toogleButton != null) {
+                var langToggle = document.createElement("a");
+                langToggle.href = "#"
+                langToggle.innerText = "language toggle";
+                langToggle.onclick = function () {
+                    toogleButton.click();
+                };
+                container.appendChild(langToggle);
+            }
+
+            for (var i = 0; i < langs.length; i++) {
+                var lang = langs[i];
+                pathParts[1] = lang;
+                var a = document.createElement("a");
+                a.href = currentLang == lang ? "#" : location.origin + pathParts.join("/");
+                a.innerText = lang;
+                container.appendChild(a);
+            }
+        }
+    })();
+})();
